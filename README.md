a1ad9d652d05c32b28eba9a626b7bc8f250e04ef
tornord <tornord@gmail.com>
2023-03-06 13:53:57 +0100

    readme

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ggbbcq)
 
-# To run the test
+## Scope and problem formulation
+
+We are running a Marko / Express.js setup today. We want to convert from cjs to esm js and use Vite build tool.
+
+The Marko server side rendering is done like this `template.renderToString(viewModel, errHtmlCallbackFunction);`
+
+Vite seems to add a <script> before and after our template html. Any idea how we can get rid of the unwanted script tags?
+
+## To run the test
 
 ```
 npm ci
--- test/renderToStringTest.js
+++ test/renderToStringTest.js
@@ -1,9 +1,7 @@
 import eventItemTemplate from '../views/components/event-item.marko';
 
 // ugly workaround
-function renderToString(template, viewModel) {
-  const res = template.render(viewModel);
-  let html = res.stream._content;
+function cleanupHtml(html) {
   html = html.replace(
     /^<script type="module" src="\/@vite\/client"><\/script>/,
     ''
@@ -15,35 +13,36 @@ function renderToString(template, viewModel) {
   return html;
 }
 
+async function renderToString(template, viewModel) {
+  const html = await new Promise((resolve, reject) => {
+    eventItemTemplate.renderToString({ eventItem }, (err, html) => {
+      if (err) reject(err);
+      else resolve(html);
+    });
+  });
+  return html;
+}
+
+const eventItem = {
+  url: 'https://www.bonniernewsevents.se/d/lrqhp8',
+  headline: 'Möten & Koncept Intranät',
+  id: 'aa7f4087-f5d9-4284-809e-4a44b17c858d',
+  startDate: '2021-01-01T08:00:00.000Z',
+  endDate: '2030-01-01T16:00:00.000Z',
+  format: 'In-person',
+  status: 'Active',
+  note: '',
+};
+
 describe('renderToString', () => {
-  it('should render html', () => {
-    const eventItem = {
-      url: 'https://www.bonniernewsevents.se/d/lrqhp8',
-      headline: 'Möten & Koncept Intranät',
-      id: 'aa7f4087-f5d9-4284-809e-4a44b17c858d',
-      startDate: '2021-01-01T08:00:00.000Z',
-      endDate: '2030-01-01T16:00:00.000Z',
-      format: 'In-person',
-      status: 'Active',
-      note: '',
-    };
-    const html = eventItemTemplate.renderToString({ eventItem });
-    console.log('empty html', html);
-    expect(html).to.not.equal(''); // Fails!
+  it('should render html without script tags', async () => {
+    const html = await renderToString(eventItemTemplate, { eventItem });
+    console.log(html);
+    expect(/script/.test(html)).to.equal(false);
   });
 
-  it('workaround', () => {
-    const eventItem = {
-      url: 'https://www.bonniernewsevents.se/d/lrqhp8',
-      headline: 'Möten & Koncept Intranät',
-      id: 'aa7f4087-f5d9-4284-809e-4a44b17c858d',
-      startDate: '2021-01-01T08:00:00.000Z',
-      endDate: '2030-01-01T16:00:00.000Z',
-      format: 'In-person',
-      status: 'Active',
-      note: '',
-    };
-    const html = renderToString(eventItemTemplate, { eventItem });
-    expect(html).to.not.equal('');
+  it('workaround', async () => {
+    const html = await renderToString(eventItemTemplate, { eventItem });
+    expect(/script/.test(cleanupHtml(html))).to.equal(false);
   });
 });
